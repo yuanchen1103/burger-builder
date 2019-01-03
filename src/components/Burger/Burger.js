9da5694a6829c50f7ad7f02ddd3584f7846085a9
yuanchen <yuanchen1103@gmail.com>
2019-01-03 22:46:26 +0800

    [add] dynamic ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,15 +2,26 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = () => {
+const burger = ({ ingredients }) => {
+   let tfIngredient = Object.keys(ingredients)
+      .map(igKey => {
+         return [...Array(ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={`${igKey}${i}`} type={igKey}/>;
+         });
+      })
+      .reduce((arr, el) => {
+         return arr.concat(el);
+      }, [])
+
+   if (!tfIngredient.length) tfIngredient = <p>Pease start adding ingredients!</p>
    return (
       <div className={classes.Burger}>
          <BurgerIngredient type="bread-top"/>
-         <BurgerIngredient type="cheese"/>
-         <BurgerIngredient type="meat"/>
+            {tfIngredient}
          <BurgerIngredient type="bread-bottom"/>
       </div>
    );
+   
 }
 
 export default burger;
